c00c927531407af41ee356dc830c6da251ce7d2e
Maouuusamaaa <Muhammad.faiqazkapermana999@gmail.com>
2026-09-21 10:53:03 +0700

    chore: bump app version for inference instrumentation

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.maouuusama.ai.device.optimizer"
         minSdk = 29
         targetSdk = 36
-        versionCode = 2
-        versionName = "0.1.2"
+        versionCode = 3
+        versionName = "0.1.3"
 
         externalNativeBuild {
             cmake {
